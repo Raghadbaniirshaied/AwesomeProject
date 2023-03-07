4c6320f5723214134408e80958ac3f51fd3d5441
test <raghadbaniirshaied99@gmail.com>
2023-03-07 13:39:56 +0300

    3

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React,{useState} from 'react';
 import { StyleSheet, Text, View ,TextInput} from 'react-native';
 
 export default function App() {
-  
 const [people, setpeople] = useState([
   { name: 'shaun', key:'1'},
   { name: 'raghad', key:'2'},

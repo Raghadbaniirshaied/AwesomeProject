39491890ba79720772c4a939cf78ab6f7fb56e62
test <raghadbaniirshaied99@gmail.com>
2023-03-09 12:33:18 +0300

    My Todos

--- App.js
+++ App.js
@@ -1,43 +1,33 @@
 import React,{useState} from 'react';
-import { StyleSheet, Text, View ,TextInput, FlatList,TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View ,FlatList} from 'react-native';
+import Header from'./components/header';
 
 export default function App() {
-const [people, setpeople] = useState([
-  { name: 'shaun', key:'1'},
-  { name: 'raghad', key:'2'},
-  { name: 'yoshi', key:'3'},
-  { name: 'mario', key:'4'},
-  { name: 'luigi', key:'5'},
-  { name: 'peach', key:'6'},
-  { name: 'suzi', key:'7'},
-  
-]);
+  const[todos, setTodos]=useState([
+    {text:'buy coffe',key:'1'},
+    {text:'create an App',key:'2'},
+    {text:'play on the switch',key:'3'}
+  ]);
 
-const pressHandler = (id) => {
-  console.log(id);
-  setPeople((prevPeople) => {
-    return prevPeople.filter(person => person.id !=id)
-
-  })
-}
-
-
-  return (
-    <View style={styles.container}>
-
-      <FlatList
-        numColumns={2}
-        keyExtractor={(item) => item.id}
-        date={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-          
-          <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-          
+return(
+     <View style={styles.container}>
+     <Header />
+      <View style={styles.content}>
+       {/* to form*/}
+       <View style={styles.list}>
+        <FlatList
+        data={todos}
+        renderItem={({item}) => (
+       <Text>{item.text}</Text>
         )}
+         
         />
+
+       </View>
+      </View>
+
      </View>
+
      
 
   );
@@ -47,9 +37,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20,
+    
   },
+  content: {
+    padding: 40,
+
+  },
+  list: {
+    marginTop: 20,
+  }
  
 });
 
